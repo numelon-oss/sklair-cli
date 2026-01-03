7e5cb3b43bd30b5a2cc3564a77154bef99fe160e
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2026-01-03 22:38:15 +0000

    Add GitHub build action + release notes for v0.1.0

--- constants/version.go
+++ constants/version.go
@@ -1,20 +1,8 @@
 package constants
 
+// these are replaced by ldflags during compile time on release
 var (
-	Version   = "0.1.0"   // to be replaced by ldflags during compile time in a GitHub runner
-	Commit    = "unknown" // likewise
-	BuildDate = "unknown" // likewise, ISO-8601 UTC
+	Version   = "development-build" // `MAJOR.MINOR.PATCH`, semantic versioning
+	Commit    = "unknown commit"
+	BuildDate = "unknown date" // `YYYY-MM-DD at HH:MM:SS`
 )
-
-/*
-VERSION=v0.0.0
-COMMIT=$(git rev-parse --short HEAD)
-DATE=$(date -u +%Y-%m-%dT%H:%M:%SZ)
-
-go build \
-  -ldflags "\
-    -X sklair/constants.Version=$VERSION \
-    -X sklair/constants.Commit=$COMMIT \
-    -X sklair/constants.BuildDate=$DATE \
-  "
-*/
